e19329e113eebc095176897607d4446dda9efc32
John182-dev <77970404+John182-dev@users.noreply.github.com>
2021-07-07 20:14:48 -0300

    Semaforo
    js

---
+++ script.js
@@ -0,0 +1,14 @@
+let sinal = 'verde'
+
+switch(sinal) {
+  case 'verde' :
+  console.log('Pode passar')
+  break
+  case 'amarelo' :
+  console.log('Espere')
+  break
+  case 'vermelho' :
+  console.log('Pare')
+  default:
+  console.log("Invalido")
+}
